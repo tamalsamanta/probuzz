7bd55dd8650c733e7849f569a11ff35b17e5d4a6
tamalsamanta <tamalsamanta2601@gmail.com>
2021-04-12 22:58:51 +0530

    Index Done

--- script/script.js
+++ script/script.js
@@ -1,22 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll('.navbar-burger'),
-    0
-  )
-
-  if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach((el) => {
-      el.addEventListener('click', () => {
-        const target = el.dataset.target
-        const $target = document.getElementById(target)
-        el.classList.toggle('is-active')
-        $target.classList.toggle('is-active')
-      })
-    })
-  }
+$(document).ready(function () {
+  $(this).scrollTop(0)
 })
+
+$(function () {
+  $('html, body').css({
+    overflowY: 'hidden',
+  })
+  setTimeout(function () {
+    $('html, body').css({
+      overflowY: 'auto',
+    })
+    // .animate(
+    //   {
+    //     scrollTop: $('.is-max-desktop').offset().top,
+    //   },
+    //   2000
+    // )
+  }, 4500)
+})
+
+joinUs = () => {
+  window.location.href = 'https://forms.gle/qtmhPxvRY8JHovUaA' //google form link
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const $navbarBurgers = Array.prototype.slice.call(
+//     document.querySelectorAll('.navbar-burger'),
+//     0
+//   )
+
+//   if ($navbarBurgers.length > 0) {
+//     $navbarBurgers.forEach((el) => {
+//       el.addEventListener('click', () => {
+//         const target = el.dataset.target
+//         const $target = document.getElementById(target)
+//         el.classList.toggle('is-active')
+//         $target.classList.toggle('is-active')
+//       })
+//     })
+//   }
+// })
 
 ScrollReveal().reveal('.welcome')
 ScrollReveal().reveal('.wcCard', { interval: 300 })
 ScrollReveal().reveal('.services')
 ScrollReveal().reveal('.svcCard', { interval: 300 })
+ScrollReveal().reveal('.team')
+ScrollReveal().reveal('.member', { interval: 300 })
